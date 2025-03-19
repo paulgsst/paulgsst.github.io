a5afaabd5827c50925d276dcf51248429df048f5
Paul Gresset <paulgresset3@gmail.com>
2025-03-19 16:33:25 +0100

    Ajout d'un logo

--- Include/entete.php
+++ Include/entete.php
@@ -2,6 +2,7 @@
 <html lang="fr">
 <head>
   <title>Portfolio Gresset Paul test</title>
+  <link rel="icon" href="Images/IMG_2429.ico">
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
