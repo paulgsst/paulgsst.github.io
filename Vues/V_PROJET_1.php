857fb78ed108ed4fe42801f8d11d68d7dc0d5da8
Paul Gresset <paulgresset3@gmail.com>
2025-03-20 11:05:33 +0100

    update projet 1

--- Vues/V_PROJET_1.php
+++ Vues/V_PROJET_1.php
@@ -12,25 +12,45 @@
     C'est un magasin de téléphonie situé à Beaucaire (30300) .
     Il est spécialisé dans la vente de téléphones portables neufs ou reconditionnés et d'accessoires.
   </p>
-  <img src="Images/magasin.jpg" id="imgMagasin">
+  <a id="cliquezPourAgrandir" href = "Images/magasin.jpg" target="_blank"><img src="Images/magasin.jpg" id="imgMagasin"></a>
+  <h6 id = "cliquezPourAgrandir">Cliquez pour agrandir</h6>
+  <img src = "Images/magasin.jpg" id = "imgMagasinGrand">
   <p>
     J'ai effectué mon stage chez eux du 13 Mai 2024 au 15 Juin 2024 . 
     Lors de mon arrivé , on m'a fais la visite du magasin et des ateliers de réparation . 
     Ensuite on a parlé du projet de l'application , j'ai commencé par faire le modèle conceptuel des données afin de structurer la base de donnéees . 
 </br>
 </br>
+    Voici un diagramme de Gantt pour mieux comprendre le déroulé du stage
+    <p>
+      <a id="cliquezPourAgrandir" href = "Images/gantt-stage-1.PNG" target="_blank"><img src = "Images/gantt-stage-1.PNG" id="ganttStage1"></a>
+      <h6 id = "cliquezPourAgrandir">Cliquez pour agrandir</h6>
+      <img src = "Images/gantt-stage-1.PNG" id="ganttStage1Grand">
+      </p>
+</br>
+</br>
     Voici donc un apperçu du schéma de la base de données : 
   </p>
   <img src="Images/mld.PNG" id="mld">
-<p>
-  Ensuite , j'ai commencé à développer l'application sur Visual Studio 2022 en utilisant le langage C# (C Sharp)
+  <h6 id = "cliquezPourAgrandir">Cliquez pour agrandir</h6>
+  <p>
+  Ensuite , j'ai commencé à développer l'application sur Visual Studio 2022 en utilisant le langage C# (C Sharp),
+  c'est un choix de ma part car c'était le  langage avec lequel j'étais le plus à l'aise à ce moment là .
 </br>
   Le menu principal se présente comme suivant : 
 </p>
 <img src="Images/projet1.png" id = "imgMenuApplication"/>
+<h6 id = "cliquezPourAgrandir">Cliquez pour agrandir</h6>
 <p>
   Il y a donc une partie destinée à la gestion de la base de données . 
 </br>
-  (Voici des exemples des menus de gestion)
+  Il faut noter que l'aspect esthétique à été mit de coté afin de me concentrer sur l'efficacité de l'application ,
+  car mon temps était limité . 
 </p>
-</div>
+</br>
+  <p>
+    Ci-dessous un exemple d'une classe objet de l'application : 
+  </p>
+  <img src = "Images/classeCSharpExemple.png" id = "classeCSharpExemple" />
+  <h6 id = "cliquezPourAgrandir">Cliquez pour agrandir</h6>
+  </div>
--- Vues/v_accueil_projets.php
+++ Vues/v_accueil_projets.php
@@ -2,8 +2,8 @@
         <h2>Mes Projets</h2>
         <div class="projet">
             <a href="index.php?gestion=<?php echo C_PROJET?>&action=<?php echo V_PROJET1?>"><img src="Images/logoreconnect.png" alt="Projet 1"></a>
-            <h3>Projet 1</h3>
-            <p>Description du projet 1.</p>
+            <h3>Stage 1ère année BTS SIO SLAM</h3>
+            <p>Projet Fournisseur Reconnect</p>
         </div>
         <div class="projet">
             <img src="images/projet2.jpg" alt="Projet 2">
