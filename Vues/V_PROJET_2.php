c6b983bd56ea6550cf15eb43bc8d99c6235011ca
Paul Gresset <paulgresset3@gmail.com>
2025-03-25 23:28:28 +0100

    Description stage 2eme année BTS SIO SLAM

--- Vues/V_PROJET_2.php
+++ Vues/V_PROJET_2.php
@@ -1,2 +1,53 @@
 <div id = "description">
+    <p>
+        Durant mon stage de 2eme année de BTS SIO SLAM , j'ai été dans une entreprise spécialisée dans la publicité numérique .
+    </p>
+</div>
+<div class='imgMenu'>
+  <img id='imgMenuFleche' src="Images/fleche_bas.png">
+</div>
+<div id="descriptionMagasin">
+    <p>
+        Cette entreprise est située aux Docks à la Joliette à Marseille .
+    </p>
+    <img src="Images/stage2/les-docks.jpg" id="imgLesDocks" alt="">
+    <p>
+        Le nom de cette entreprise est Biggie Group 
+    </p>
+    <img src="Images/stage2/biggiegroup_logo.jpg" id="logoBiggie" alt="">
+    <p>
+        Durant ce stage on m'a confié des ticket sur différents projet . 
+        Ces tickets me demandaient de faire des modifications front-end la plupart du temps
+    </p>
+    <p>
+        Je vais vous montrer un exemple de mission que l'on m'a confier :
+    </p>
+    <img src="Images/stage2/datachecker.png" id="imgDataChecker" alt="">
+    <p>
+        J'ai du réaliser ce tableau permettant de voir les différentes données liées au publicités . 
+        Ce tableau à été fait avec Vue.js et Vuetify qui sont des framework JavaScript . 
+        L'extrait de code ci dessous est celui du tableau :
+    </p>
+    <img src="Images/stage2/codeDatachecker.png" id="imgTabDatachecker" alt="">
+    <p>
+        Et voici la fenêtre de modification/ajout de données dont le code est encore en dessous : 
+    </p>
+    <img src="Images/stage2/tabModifDatachecker.png" id="imgTabModifDatachecker" alt="">
+    <br>
+    <img src="Images/stage2/codeModifDatachecker.png" id="imgCodeModifDatachecker" alt="">
+    <p>
+        Et enfin voici le code permettant d'ouvrir cette fenêtre de modification et d'enregistrer les nouvelles données :
+    </p>
+    <img src="Images/stage2/codeOuvrirMenuModif.png" id="imgCodeOuvrirMenuModif"  alt="">
+    <br>
+    <img src="Images/stage2/codeSaveModifDatachecker.png" id="imgCodeSaveModifDatachecker" alt="">
+    <p>
+        L'équipe de développement de l'entreprise travaille selon la méthode agile . 
+        <br>
+        Le matin on se réunnissait pour parler de ce que l'on avait fait la veille ,
+        <br> si on avait eu des problèmes , si ces dit problèmes ont été résolus 
+        <br> et qu'est-ce qu'on allait faire aujourd'hui . 
+        <br>
+        Ils faisaient souvent des réunions en visions conférences pour parler de l'avancé des projets .
+    </p>
 </div>
--- Vues/v_accueil_projets.php
+++ Vues/v_accueil_projets.php
@@ -6,9 +6,9 @@
             <p>Projet Fournisseur Reconnect</p>
         </div>
         <div class="projet">
-            <a href="index.php?gestion=<?php echo C_PROJET?>&action=<?php echo V_PROJET2?>"><img src="images/projet2.jpg" alt="Projet 2"></a>
-            <h3>Projet 2</h3>
-            <p>Description du projet 2.</p>
+            <a href="index.php?gestion=<?php echo C_PROJET?>&action=<?php echo V_PROJET2?>"><img src="Images/stage2/biggiegroup_logo.jpg" alt="Projet 2"></a>
+            <h3>Stage 2ème année BTS SIO SLAM</h3>
+            <p>Biggie Group</p>
         </div>
         <!-- Ajouter d'autres projets ici -->
     </section>
