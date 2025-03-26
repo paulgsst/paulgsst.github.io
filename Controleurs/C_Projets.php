7c8ef10729f7142a883023d62f0af492c11994b7
Paul <paulgresset3@gmail.com>
2025-03-26 10:48:44 +0100

    Update C_Projets.php

--- Controleurs/C_Projets.php
+++ Controleurs/C_Projets.php
@@ -9,19 +9,19 @@ switch($_REQUEST['action']){
     case V_PROJET_ACCUEIL:
         require "Include/entete.php";
         require "Vues/v_accueil_projets.php";
-        require "Include/pied.php";
+
         break;
 
     case V_PROJET1:
         require "Include/entete.php";
         require "Vues/V_PROJET_1.php";
-        require "Include/pied_projet.php";
+
         break;
 
     case V_PROJET2:
         require "Include/entete.php";
         require "Vues/V_PROJET_2.php";
-        require "Include/pied.php";
+
         break;
 
 }
