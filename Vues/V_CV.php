2bd783d2dd39f30a3e022cec36be05efb3c0aa86
Paul <paulgresset3@gmail.com>
2025-06-13 13:33:50 +0200

    Update V_CV.php
    Modification du bouton de téléchargement du tableau de synthèse

--- Vues/V_CV.php
+++ Vues/V_CV.php
@@ -9,6 +9,6 @@
 <h2>Voici mon tableau de synthèse</h2>
 <embed src="./Sources/tableau_de_synthèse.pdf" width="600" height="700" type="application/pdf" id="CV"/>
 <br>
-<button onClick="telechargerTS()">Télécharger mon CV</button>
+<button onClick="telechargerTS()">Télécharger mon tableau de synthèse</button>
 
 </div>
